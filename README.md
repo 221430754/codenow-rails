40e871f35978861522fb787d2bec8f52d4151d12
angelina martinez <angmartinez11@gmail.com>
2018-05-12 15:57:00 +0000

    Todo app

---
+++ README.md
@@ -0,0 +1,6 @@
+# Rails todo app 
+Route (routes.rb)
+Controller (TodosController)
+Action (index, show)
+View (app/views/todos/index.erb)
+
---
+++ app/controllers/todos_controller.rb
@@ -0,0 +1,34 @@
+class TodosController<ApplicationController 
+    def index 
+    
+        
+    end 
+    def show 
+        id = params[:id]
+        if id== '1'
+            @todo= {
+            title: 'U.S. history',
+            description: 'midtermweekprep',
+            }
+        elsif id == '2'
+            @todo= {
+                title: 'English',
+                description: 'research essay',
+            }
+        elsif id == '3'
+        
+            @todo= {
+                title: 'physics',
+                description: 'study for final',
+            }
+        else 
+            @todo= {
+                title: 'house chores',
+                description: 'clean room',
+            }
+            
+         end 
+    
+     end  
+ end 
+    
